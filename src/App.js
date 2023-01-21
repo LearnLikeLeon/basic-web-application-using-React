775d0489b31eadd5fd70a8c0ed07d3690da7a391
LearnLikeLeon <leon@learnlikeleon.com>
2023-01-21 10:05:36 -0500

    Changed the logo with a beautiful image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
-import Logo from './Learn Like Leon_Logo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Logo} className="App-logo-1" alt="Logo" />
+        <img src="https://images.unsplash.com/photo-1489516408517-0c0a15662682?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" className="App-logo-1" alt="Coming soon" />
+        
+        
         <p className='text-bold'>
-          This free online training program will empower <code>YOU, me and US</code> with hands-on knowledge and practical application of 
+          This free online training program will empower us with hands-on knowledge and practical application of 
           cloud computing.
         </p>
         <a
